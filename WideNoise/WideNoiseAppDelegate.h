49225092742cd1916db60fbb75aaac4f679177e9
Emilio Pavia <emilio.pavia@gmail.com>
2011-08-23 15:03:16 +0200

    Create project skeleton

---
+++ WideNoise/WideNoiseAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WideNoiseAppDelegate.h
+//  WideNoise
+//
+//  Created by Emilio Pavia on 23/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WideNoiseAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end

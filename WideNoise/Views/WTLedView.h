277378fb056c7bd85a99dcdb286064fd9ca138e7
Emilio Pavia <emilio.pavia@gmail.com>
2011-09-02 15:14:31 +0200

    Add WTLedView clas used to display the noise graph

---
+++ WideNoise/Views/WTLedView.h
@@ -0,0 +1,27 @@
+//
+//  WTLedView.h
+//  WideNoise
+//
+//  Created by Emilio Pavia on 01/09/11.
+//  Copyright 2011 WideTag, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WTLedView;
+
+@protocol WTLedViewDataSource <NSObject>
+
+- (CGFloat)ledView:(WTLedView *)ledView valueForColumnAtIndex:(NSUInteger)index;
+
+@end
+
+@interface WTLedView : UIView
+
+@property (nonatomic, assign) id <WTLedViewDataSource> dataSource;
+@property (nonatomic, retain) UIColor *ledColor;
+
+@property (nonatomic, readonly) NSUInteger numberOfCols;
+@property (nonatomic, readonly) NSUInteger numberOfRows;
+
+@end
---
+++ WideNoise/Views/WTLedView.m
@@ -0,0 +1,71 @@
+//
+//  WTLedView.m
+//  WideNoise
+//
+//  Created by Emilio Pavia on 01/09/11.
+//  Copyright 2011 WideTag, Inc. All rights reserved.
+//
+
+#import "WTLedView.h"
+
+#define PIXEL_WIDTH 2
+#define PIXEL_PITCH 1
+#define COL_WIDTH (PIXEL_WIDTH + PIXEL_PITCH)
+
+@implementation WTLedView
+
+@synthesize ledColor = _ledColor;
+@synthesize dataSource = _dataSource;
+
+#pragma mark - Properties
+
+- (NSUInteger)numberOfCols
+{
+    return (NSUInteger)(self.frame.size.width / COL_WIDTH);
+}
+
+- (NSUInteger)numberOfRows
+{
+    return (NSUInteger)(self.frame.size.height / COL_WIDTH);
+}
+
+#pragma mark -
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.ledColor = [UIColor redColor];
+    }
+    return self;
+}
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetLineWidth(context, 2);
+    CGContextSetStrokeColorWithColor(context, self.ledColor.CGColor);
+    CGFloat dashArray[] = {PIXEL_WIDTH, PIXEL_PITCH};
+    CGContextSetLineDash(context, 0, dashArray, 2);
+    
+    NSUInteger cols = self.numberOfCols;
+    NSUInteger rows = self.numberOfRows;
+    for (int i=1; i<=cols; i++) {
+        CGContextMoveToPoint(context, COL_WIDTH*i, self.frame.size.height);
+        CGContextAddLineToPoint(context, COL_WIDTH*i, self.frame.size.height - ((int)(rows*[self.dataSource ledView:self valueForColumnAtIndex:i-1]) * COL_WIDTH));
+        
+        CGContextStrokePath(context);
+    }
+}
+
+- (void)dealloc
+{
+    [_ledColor release];
+    [super dealloc];
+}
+
+@end

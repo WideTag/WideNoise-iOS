303a001fc08277c40fca3940e3ad273e054d28a0
Emilio Pavia <emilio.pavia@gmail.com>
2011-09-02 15:21:29 +0200

    Add new unit test

--- WideNoiseTests/WideNoiseTests.m
+++ WideNoiseTests/WideNoiseTests.m
@@ -8,6 +8,7 @@
 
 #import "WideNoiseTests.h"
 
+#import "NSData+Base64.h"
 #import "WTNoise.h"
 
 @implementation WideNoiseTests
@@ -40,7 +41,20 @@
     STAssertEquals([(NSNumber *)[noise.samples lastObject] floatValue], (float)120.0, @"Out of bounds level assignment failed!");
     
     STAssertEquals(noise.averageLevel, (float)60.0, @"Average level calculation failed!");
-   
+}
+
+- (void)testBase64Encoder
+{
+    NSString *testString = @"This is a test for the NSData Base64 encoding/decoding";
+    NSData *testData = [[NSData alloc] initWithBytes:[testString cStringUsingEncoding:NSUTF8StringEncoding] length:testString.length];
+    
+    NSData *decodedData = [NSData dataWithBase64EncodedString:[testData base64Encoding]];
+    NSString *decodedString = [[NSString alloc] initWithBytes:decodedData.bytes length:decodedData.length encoding:NSUTF8StringEncoding];
+    
+    NSAssert([testString isEqualToString:decodedString], @"Base64 encoding test failed!");
+    
+    [decodedString release];
+    [testData release];
 }
 
 @end

fdd7aa8d2ef385b2ba5fd862029b04b4a88fa725
Emilio Pavia <emilio.pavia@gmail.com>
2011-09-07 09:12:27 +0200

    Add initial implementation of ListenViewController

--- WideNoise/View Controllers/ListenViewController.h
+++ WideNoise/View Controllers/ListenViewController.h
@@ -8,16 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
+#import <CoreLocation/CoreLocation.h>
+
+#import "WTLedView.h"
 #import "WTNoiseRecorder.h"
 
-@interface ListenViewController : UIViewController <WTNoiseRecorderDelegate>
+@interface ListenViewController : UIViewController <CLLocationManagerDelegate, UIScrollViewDelegate, WTLedViewDataSource, WTNoiseRecorderDelegate>
 
+@property (nonatomic, retain) IBOutlet UIImageView *bgView;
+@property (nonatomic, retain) IBOutlet UIImageView *meterView;
+@property (nonatomic, retain) IBOutlet UIImageView *pageView;
+@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+
+@property (nonatomic, retain) IBOutlet UIView *samplingView;
+@property (nonatomic, retain) IBOutlet UIImageView *recordView;
+@property (nonatomic, retain) IBOutlet UIImageView *locationView;
+@property (nonatomic, retain) IBOutlet WTLedView *ledView;
+@property (nonatomic, retain) IBOutlet UILabel *dbLabel;
+@property (nonatomic, retain) IBOutlet UILabel *descriptionLabel;
 @property (nonatomic, retain) IBOutlet UIButton *takeButton;
 @property (nonatomic, retain) IBOutlet UIButton *extendButton;
 @property (nonatomic, retain) IBOutlet UIButton *qualifyButton;
 
-- (IBAction)takeNoiseSample:(id)sender;
-- (IBAction)extendSampling:(id)sender;
-- (IBAction)qualifyNoise:(id)sender;
+@property (nonatomic, retain) IBOutlet UIView *qualifyView;
+
+- (IBAction)action:(id)sender;
+- (IBAction)clear:(id)sender;
+- (IBAction)setType:(id)sender;
+- (IBAction)sendReport:(id)sender;
 
 @end
--- WideNoise/View Controllers/ListenViewController.m
+++ WideNoise/View Controllers/ListenViewController.m
@@ -8,51 +8,247 @@
 
 #import "ListenViewController.h"
 
+#import <AudioToolbox/AudioServices.h>
+
+#import "WTRequestFactory.h"
+
 @interface ListenViewController ()
 
 @property (nonatomic, retain) WTNoiseRecorder *noiseRecorder;
+@property (nonatomic, retain) CLLocationManager *locationManager;
+@property (nonatomic, assign) NSTimer *samplingTimer;
+
+@property (nonatomic, retain) CLLocation *currentLocation;
+
+- (void)scrollToPage:(NSUInteger)page;
+- (void)updateLocation;
 
 @end
 
 @implementation ListenViewController
 
+@synthesize bgView = _bgView;
+@synthesize meterView;
+@synthesize pageView = _pageView;
+@synthesize scrollView = _scrollView;
+@synthesize samplingView = _samplingView;
+@synthesize recordView;
+@synthesize locationView;
+@synthesize ledView = _ledView;
+@synthesize dbLabel;
+@synthesize descriptionLabel;
 @synthesize takeButton;
 @synthesize extendButton;
 @synthesize qualifyButton;
+@synthesize qualifyView;
 
 @synthesize noiseRecorder = _noiseRecorder;
+@synthesize locationManager = _locationManager;
+@synthesize samplingTimer;
+
+@synthesize currentLocation;
 
 #pragma marl - IBAction methods
 
-- (IBAction)takeNoiseSample:(id)sender
+- (IBAction)action:(id)sender
 {
-    self.noiseRecorder = [[WTNoiseRecorder alloc] init];
+    [(UIButton *)sender setSelected:YES];
+    if (sender == self.qualifyButton) {
+        [self scrollToPage:1];
+        return;
+    }
+    
+    self.takeButton.userInteractionEnabled = NO;
+    self.extendButton.userInteractionEnabled = NO;
+    self.extendButton.userInteractionEnabled = NO;
+    self.recordView.hidden = NO;
+    
+    [self.noiseRecorder recordForDuration:5];
+    [self updateLocation];
+}
+
+- (IBAction)clear:(id)sender
+{
+    self.noiseRecorder = [[[WTNoiseRecorder alloc] init] autorelease];
     self.noiseRecorder.delegate = self;
-    self.noiseRecorder.samplesPerSecond = 1;
+    self.noiseRecorder.samplesPerSecond = 20;
     
-    [self.noiseRecorder recordForDuration:10];
+    self.meterView.image = [UIImage imageNamed:@"noise_meter.png"];
+    self.dbLabel.text = @"";
+    self.descriptionLabel.text = @"";
 }
 
-- (IBAction)extendSampling:(id)sender
+- (IBAction)setType:(id)sender
 {
+    UIButton *typeButton = (UIButton *)sender;
+    NSString *type = nil;
+    switch (typeButton.tag) {
+        case 10:
+            type = @"natural";
+            break;
+        case 11:
+            type = @"artificial";
+            break;
+        case 12:
+            type = @"lovable";
+            break;
+        case 13:
+            type = @"hurting";
+            break;
+        case 14:
+            type = @"indoor";
+            break;
+        case 15:
+            type = @"outdoor";
+            break;
+        case 16:
+            type = @"single";
+            break;
+        case 17:
+            type = @"multiple";
+            break;
+        default:
+            break;
+    }
     
+    if ([typeButton isSelected]) {
+        [typeButton setSelected:NO];
+    } else {
+        [typeButton setSelected:YES];
+    }
 }
 
-- (IBAction)qualifyNoise:(id)sender
+- (IBAction)sendReport:(id)sender
 {
+    [(UIButton *)sender setSelected:YES];
+    [[[[UIAlertView alloc] initWithTitle:@"Beta Release" 
+                               message:@"This feature is not yet implemented!" 
+                              delegate:nil 
+                     cancelButtonTitle:@"OK" 
+                     otherButtonTitles:nil] autorelease] show];
+}
+
+#pragma mark - Private methods
+
+- (void)scrollToPage:(NSUInteger)page
+{
+    [self.scrollView scrollRectToVisible:CGRectMake(self.scrollView.frame.size.width * page, 
+                                                    0, 
+                                                    self.scrollView.frame.size.width, 
+                                                    self.scrollView.frame.size.height) 
+                                animated:YES];
+    NSString *imageName = [NSString stringWithFormat:@"page_%d.png", page+1];
+    self.pageView.image = [UIImage imageNamed:imageName];
+}
+
+- (void)updateLocation
+{
+    if (![CLLocationManager locationServicesEnabled]) {
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Location Services" 
+                                                            message:@"You must enable location services in your device settings in order to use this application." 
+                                                           delegate:nil 
+                                                  cancelButtonTitle:@"OK" 
+                                                  otherButtonTitles:nil];
+        [alertView show];
+        [alertView release];
+    } else {
+        [self.locationManager startUpdatingLocation];
+    }
+}
+
+#pragma marl - CoreLocation delegate methods
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    if (newLocation.horizontalAccuracy >= 0 && newLocation.horizontalAccuracy < 100) {
+        self.currentLocation = newLocation;
+        [manager stopUpdatingLocation];
+        
+        self.locationView.hidden = NO;
+    }    
+}
+
+#pragma mark - WTLedViewDataSource methods
+
+- (CGFloat)ledView:(WTLedView *)ledView valueForColumnAtIndex:(NSUInteger)index
+{
+    WTNoise *noise = self.noiseRecorder.recordedNoise;
+    NSUInteger totalSamples = self.noiseRecorder.samplesPerSecond * self.noiseRecorder.recordingDuration;
+    if (totalSamples == 0) {
+        return 0;
+    }
     
+    float len = totalSamples / (float)ledView.numberOfCols;
+    float level = 0.0;
+    for (NSUInteger i=0; i<ceilf(len); i++) {
+        NSUInteger j = (int)(len*index) + i;
+        if (j < [noise.samples count]) {
+            level += [(NSNumber *)[noise.samples objectAtIndex:j] floatValue];
+        } else {
+            return 0;
+        }
+    }
+    
+    return level / (120.0 * ceil(len));
+}
+
+#pragma mark - UIScrollViewDelegate methods
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    NSUInteger page = floorf(CGRectGetMinX(scrollView.bounds) / CGRectGetWidth(scrollView.bounds)) + 1;
+    NSString *imageName = [NSString stringWithFormat:@"page_%d.png", page];
+    self.pageView.image = [UIImage imageNamed:imageName];
 }
 
 #pragma mark - WTNoiseRecorderDelegate methods
 
-- (void)noiseRecorder:(WTNoiseRecorder *)noiseRecorder didRecordSampleWithLevel:(float)level
-{
-    NSLog(@"level: %f", level);
+- (void)noiseRecorder:(WTNoiseRecorder *)noiseRecorder didUpdateNoise:(WTNoise *)noise
+{        
+    [self.ledView setNeedsDisplay];  
 }
 
-- (void)noiseRecorder:(WTNoiseRecorder *)noiseRecorder didFinishRecordingNoise:(WTNoise *)noise
+- (void)noiseRecorderDidFinishRecording:(WTNoiseRecorder *)noiseRecorder
 {
-    NSLog(@"average level: %f", noise.averageLevel);
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    self.recordView.hidden = YES;
+    
+    float db = noiseRecorder.recordedNoise.averageLevel;
+    NSString *imageName = nil;
+    NSString *description = nil;
+    if (db <= 30) {
+		imageName = @"noise_meter_1.png";
+        description = @"Feather";
+	} else if (db <= 60) {
+		imageName = @"noise_meter_2.png";
+        description = @"Sleeping Cat";
+	} else if (db <= 70) {
+		imageName = @"noise_meter_3.png";
+        description = @"TV";
+	} else if (db <= 90) {
+		imageName = @"noise_meter_4.png";
+        description = @"Car";
+	} else if (db <= 100) {
+		imageName = @"noise_meter_5.png";
+        description = @"Dragster";
+	} else if (db <= 115) {
+		imageName = @"noise_meter_6.png";
+        description = @"T-rex";
+	} else {
+		imageName = @"noise_meter_7.png";
+        description = @"Rock Concert";
+	}
+    
+    self.meterView.image = [UIImage imageNamed:imageName];
+    self.dbLabel.text = [NSString stringWithFormat:@"%ddb", (int)noiseRecorder.recordedNoise.averageLevel];
+    self.descriptionLabel.text = description;
+    
+    self.takeButton.userInteractionEnabled = YES;
+    self.extendButton.userInteractionEnabled = YES;
+    self.extendButton.userInteractionEnabled = YES;
+    
+    [self.takeButton setSelected:NO];
+    [self.extendButton setSelected:NO];
 }
 
 #pragma mark - 
@@ -76,10 +272,26 @@
 
 - (void)dealloc
 {
+    [_bgView release];
+    [_pageView release];
+    [_scrollView release];
+    [_samplingView release];
+    [recordView release];
+    [locationView release];
+    [meterView release];
+    [_ledView release];
+    [dbLabel release];
+    [descriptionLabel release];
     [takeButton release];
     [extendButton release];
     [qualifyButton release];
+    [qualifyView release];
+    
     [_noiseRecorder release];
+    [_locationManager release];
+    
+    [currentLocation release];
+    
     [super dealloc];
 }
 
@@ -88,22 +300,66 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.frame.size.width * 3, self.scrollView.frame.size.height);
+    self.samplingView.frame = CGRectMake(0, 0, self.scrollView.frame.size.width, self.scrollView.frame.size.height);
+    self.qualifyView.frame = CGRectMake(self.scrollView.frame.size.width, 0, self.scrollView.frame.size.width, self.scrollView.frame.size.height);
+    [self.scrollView addSubview:self.samplingView];
+    [self.scrollView addSubview:self.qualifyView];
+    
+    self.ledView.dataSource = self;
+    self.ledView.ledColor = [UIColor colorWithRed:1.0 green:172.0/255.0 blue:83.0/255.0 alpha:1.0];
+    
+    UIColor *ledColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"pixel_pattern.png"]];
+    self.dbLabel.textColor = ledColor;
+    self.descriptionLabel.textColor = ledColor;
+
+    self.recordView.hidden = YES;
+    self.locationView.hidden = YES;
+
+    [self clear:nil];
+    
+    self.locationManager = [[[CLLocationManager alloc] init] autorelease];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
     
+    self.bgView = nil;
+    self.pageView = nil;
+    self.scrollView = nil;
+    self.samplingView = nil;
+    self.recordView = nil;
+    self.locationView = nil;
+    self.meterView = nil;
+    self.ledView = nil;
+    self.dbLabel = nil;
+    self.descriptionLabel = nil;
     self.takeButton = nil;
     self.extendButton = nil;
     self.qualifyButton = nil;
+    self.qualifyView = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    [self.locationManager stopUpdatingLocation];
 }
 
 @end

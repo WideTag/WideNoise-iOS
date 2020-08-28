82e1fd8413c2a1ee089cbe3b9ed0ac4d6003c816
Erin Casali <folletto@gmail.com>
2020-08-28 17:11:06 +0100

    Added credit: Babele Dunnit
    Apologies, this should have happened far before.

--- WideNoise/Models/WTNoise.m
+++ WideNoise/Models/WTNoise.m
@@ -14,6 +14,7 @@
 #import "WTRequestFactory.h"
 
 // use this table to convert from raw mic level to dB SPL
+// based on research and device testing done by Babele Dunnit
 static Float32 lookup_table[][2] = {
     {0.0, 0.0},
     {0.003, 50},
